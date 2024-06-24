b28805175331371253a4f3e61b2aa4908d1daf83
rhmn27210 <rahman27210@gmail.com>
2024-06-24 20:15:43 +0700

    Add files via upload

---
+++ kodland_utils.py
@@ -0,0 +1,11 @@
+import random
+
+character = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+pass_length = int(input("Masukkan Panjang Password: "))
+password = ""
+
+for i in range(pass_length):
+    password += random.choice(character)
+
+print(password)
